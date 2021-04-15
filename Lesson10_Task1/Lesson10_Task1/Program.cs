a7e2e65fa92a4628512b9ab421c0069c3054453b
Lotus19955 <lotus19955@gmail.com>
2021-04-15 17:17:03 +0300

    Task 1

---
+++ Lesson10_Task1/Lesson10_Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Person_Initialization;
+
+namespace Lesson10_Task1
+{
+    class Program
+    {
+        //    enum CompaniesNames
+        //    EPAM = 1,
+        //    Google,
+        //    Amazon,
+        //    Apple,
+        /// <summary>
+        /// Returns information about the user
+        /// </summary>
+        /// <returns>Name; SurName; CompanyWhereUserWork; Age
+        /// </returns>
+        /// <param name="person.name">Take user 'name'</param>
+        /// <param name="person.surname">Take user 'surname'</param>
+        /// /// <param name="person.companyName">Take user 'company'</param>
+        /// /// <param name="person.age">Take user 'age'</param>
+        static User GetUser()
+        {
+            User person = new User();
+
+            person.name = "Andrei";
+            person.surname = "Yermalovich";
+            person.companyName = 2;
+            person.age = 25;
+
+            return person;
+        }
+        static void Main(string[] args)
+        {
+            
+            User person = GetUser();
+            
+            ActionWithUser.GetUsername(person);
+            ActionWithUser.GetWorkCompany(person);
+            ActionWithUser.GetAge(person);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson10_Task1/Person_Initialization/User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Initialization
+{
+    public enum CompaniesNames
+    {
+        EPAM = 1,
+        Google,
+        Amazon,
+        Apple,
+    }
+    public class User
+    {
+        public string name;
+        public string surname;
+        public int companyName;
+        //private int Age;
+        public int age;
+        //{
+        //    set
+        //    {
+        //        if (value < 18)
+        //        {
+        //            Console.WriteLine("Age must not be less than 18");
+        //        }
+
+        //        else
+        //        {
+        //            Age = value;
+        //        }
+        //    }
+        //    get { return Age; }
+        //}
+    }
+    /// <summary>
+    /// Do some actions with 'class user'
+    /// </summary>
+    public class ActionWithUser
+    {
+        /// <summary>
+        /// Write to console person 'name' and 'surname'
+        /// </summary>
+        public static void GetUsername(User person)
+        {
+            Console.WriteLine($"User: {person.name} {person.surname}");
+        }
+        /// <summary>
+        /// Write to console person 'name' and 'company' where he work
+        /// </summary>
+        public static void GetWorkCompany(User person)
+        {
+            if (person.companyName < 4)
+            {
+                Console.WriteLine($"User: {person.name} works in company: {(CompaniesNames)person.companyName}");
+            }
+            else
+            {
+                Console.WriteLine("You must select company from 1 to 4, look 'enum'");
+            }
+
+        }
+        /// <summary>
+        /// Write to console person 'name' and 'age'
+        /// </summary>
+        public static void GetAge(User person)
+        {
+            if (person.age < 18)
+            {
+                Console.WriteLine($"User: {person.name} Age: Age must not be less than 18");
+            }
+            else
+            {
+                Console.WriteLine($"User: {person.name} Age: {person.age}");
+            }
+        }
+    }
+ }
+
+
+
