c1caa9b064c784b2ea4069b1bad631bce655e631
Lotus19955 <lotus19955@gmail.com>
2021-04-17 00:20:52 +0300

    Add task 2, and fixed issues from task 1

--- Lesson10_Task1/Lesson10_Task1/Program.cs
+++ Lesson10_Task1/Lesson10_Task1/Program.cs
@@ -9,39 +9,17 @@ namespace Lesson10_Task1
 {
     class Program
     {
-        //    enum CompaniesNames
-        //    EPAM = 1,
-        //    Google,
-        //    Amazon,
-        //    Apple,
-        /// <summary>
-        /// Returns information about the user
-        /// </summary>
-        /// <returns>Name; SurName; CompanyWhereUserWork; Age
-        /// </returns>
-        /// <param name="person.name">Take user 'name'</param>
-        /// <param name="person.surname">Take user 'surname'</param>
-        /// /// <param name="person.companyName">Take user 'company'</param>
-        /// /// <param name="person.age">Take user 'age'</param>
-        static User GetUser()
+        static void Main(string[] args)
         {
             User person = new User();
 
             person.name = "Andrei";
-            person.surname = "Yermalovich";
-            person.companyName = 2;
-            person.age = 25;
+            person.surname = "Termalovich";
+            person.age = 19;
+            person.companyNumber = 2;
 
-            return person;
-        }
-        static void Main(string[] args)
-        {
-            
-            User person = GetUser();
-            
-            ActionWithUser.GetUsername(person);
-            ActionWithUser.GetWorkCompany(person);
-            ActionWithUser.GetAge(person);
+            person.Print();
+
             Console.ReadLine();
         }
     }
--- Lesson10_Task2/Woker/Woker.cs
+++ Lesson10_Task2/Woker/Woker.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Person_Initialization
+namespace Woker_Initialization
 {
+    
     public enum CompaniesNames
     {
         EPAM = 1,
@@ -13,29 +14,18 @@ namespace Person_Initialization
         Amazon,
         Apple,
     }
-
-    /// <summary>
-    /// Returns information about the user
-    /// </summary>
-    /// <returns>Name; SurName; CompanyWhereUserWork; Age
-    /// </returns>
-    /// <param name="person.name">Take user 'name'</param>
-    /// <param name="person.surname">Take user 'surname'</param>
-    /// /// <param name="person.companyName">Take user 'company'</param>
-    /// /// <param name="person.age">Take user 'age'</param>
     public class User
     {
         public string name;
         public string surname;
-        public int companyNumber;
+        public int companyName;
         public int age;
-
         public void Print()
         {
             Console.WriteLine($"User: {name} {surname}");
-            if (companyNumber < 4)
+            if (companyName < 4)
             {
-                Console.WriteLine($"Company: {(CompaniesNames)companyNumber}");
+                Console.WriteLine($"Company: {(CompaniesNames)companyName}");
             }
             else
             {
@@ -53,7 +43,4 @@ namespace Person_Initialization
             Console.WriteLine();
         }
     }
- }
-
-
-
+}
